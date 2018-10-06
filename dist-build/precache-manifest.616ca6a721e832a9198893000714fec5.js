891d6f1fe63d3e9d28c6623525fe063a80a6df9a
Bartlomiej Tyrpien <bartlomiejtyrpien@BartlomiejsMBP2.home>
2018-10-06 12:49:22 -0400

    build

---
+++ dist-build/precache-manifest.616ca6a721e832a9198893000714fec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "22ee33b847b4208a5cccc5706c627d7b",
+    "url": "/static/media/brettk.22ee33b8.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f65e6f5bc7c5c334afbd",
+    "url": "/static/js/main.f65e6f5b.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "f65e6f5bc7c5c334afbd",
+    "url": "/static/css/main.cce3b20a.chunk.css"
+  },
+  {
+    "revision": "91b7649a2b29d6684e0229b4643634d9",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+import yearbook from './brettk.png'
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="App-header container">
+          <h1 className="welcome-to-da-boof"> Welcome to Boofnation</h1>
+          <h1>All hail the Honorable <span className="boof-it-baby">Boofer</span>, Judge Brett Kavanaugh</h1>
+          <h2>The first <span className="boof-it-baby">boofer</span> to grace the Supreme Court of the United States</h2>
         </header>
+        <div className="pic-of-my-boy-brett">
+          <img src={yearbook} alt={"yearbook"} />
+        </div>
+        <div className="whitehouse-grillin">
+          <h2>Our glorious leader not betraying our secret, and answering the question like a boss</h2>
+          <iframe width="420" height="315" src="https://www.youtube.com/embed/D6yFnizey7k" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div className="colbert-killin">
+          <h2>Liberal Media on our Man's Case</h2>
+
+          <iframe width="420" height="315" src="https://www.youtube.com/embed/U2_52Dj6DsI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div><div className="snl-killin">
+          <iframe width="420" height="315" src="https://www.youtube.com/embed/VRJecfRxbr8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+        <div className = "shoot-myself-in-the-footer">
+          <p>
+            In case you didn't notice, this site is obvs satire. Copyright 2018 Boofnation.com
+          </p>
+        
+        </div>
       </div>
     );
   }
